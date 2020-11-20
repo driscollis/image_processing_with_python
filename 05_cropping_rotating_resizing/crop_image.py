97c73b7e6ae2d4e37a40a5f280480a1caefb0fa9
Mike Driscoll <mike@pythonlibrary.org>
2020-11-20 16:14:17 -0600

    Add some examples for chapter 5

---
+++ 05_cropping_rotating_resizing/crop_image.py
@@ -0,0 +1,16 @@
+# crop_image.py
+
+from PIL import Image
+
+
+def crop_image(image_path: str, coords: tuple,
+               save_location: str) -> None:
+    image_obj = Image.open(image_path)
+    cropped_image = image_obj.crop(coords)
+    cropped_image.save(save_location)
+
+
+if __name__ == "__main__":
+    crop_image("green_mantis.jpeg",
+               (302, 101, 910, 574),
+               "cropped_mantis.jpg")
---
+++ 05_cropping_rotating_resizing/rotate_image.py
@@ -0,0 +1,15 @@
+# rotate_image.py
+
+from PIL import Image
+
+
+def rotate(image_path: str, degrees_to_rotate: int,
+           saved_location: str) -> None:
+    image_obj = Image.open(image_path)
+    rotated_image = image_obj.rotate(image_obj.ROTATE_180)
+    rotated_image.save(saved_location)
+
+
+if __name__ == "__main__":
+    image = "dragonfly.jpg"
+    rotate(image, 90, "dragonfly_rotated.jpg")
