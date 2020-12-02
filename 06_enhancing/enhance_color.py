d20499ab6794c7985153c881322f51566ac2697b
Mike Driscoll <mike@pythonlibrary.org>
2020-12-02 09:07:47 -0600

    add color enhancement example

---
+++ 06_enhancing/enhance_color.py
@@ -0,0 +1,17 @@
+# enhance_color.py
+
+from PIL import Image
+from PIL import ImageEnhance
+
+
+def enhance_color(image_path: str, enhance_factor:
+                  float, output_path: str) -> None:
+    image = Image.open(image_path)
+    enhancer = ImageEnhance.Color(image)
+    new_image = enhancer.enhance(enhance_factor)
+    new_image.save(output_path)
+
+
+if __name__ == "__main__":
+    enhance_color("goldenrod_soldier_beetle.jpg", 0.0,
+                  "beetle_color_enhanced.jpg")
