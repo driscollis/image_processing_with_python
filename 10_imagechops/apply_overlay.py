aa6da222cfe5d1092c9f7467401c21944d32e1bb
Mike Driscoll <mike@pythonlibrary.org>
2021-02-03 14:32:49 -0600

    add overlay code example

---
+++ 10_imagechops/apply_overlay.py
@@ -0,0 +1,14 @@
+# apply_overlay.py
+
+from PIL import Image, ImageChops
+
+def overlay(image_path_one, image_path_two, output_path):
+    image_one = Image.open(image_path_one)
+    image_two = Image.open(image_path_two)
+    image_three = ImageChops.overlay(image_one, image_two)
+    image_three.save(output_path)
+
+
+if __name__ == "__main__":
+    overlay("shell.png", "skyline.png",
+            "overlay.jpg")
