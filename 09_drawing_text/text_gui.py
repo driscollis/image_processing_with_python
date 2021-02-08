daf48fff572c3e55e849555d5da13c0c6b6d33d1
Mike Driscoll <mike@pythonlibrary.org>
2021-02-08 12:40:30 -0600

    update GUI to fix bugs

--- 09_drawing_text/text_gui.py
+++ 09_drawing_text/text_gui.py
@@ -18,18 +18,18 @@ tmp_file = tempfile.NamedTemporaryFile(suffix=".jpg").name
 
 def get_value(key, values):
     value = values[key]
-    if value:
+    if value.isdigit():
         return int(value)
     return 0
 
 
 def apply_text(values, window):
-    image_file = values["filename"]
-    font_name = values["ttf"]
-    font_size = get_value("font_size", values)
-    color = values["colors"]
-    x, y = get_value("text-x", values), get_value("text-y", values)
-    text = values["text"]
+    image_file = values["-FILENAME-"]
+    font_name = values["-TTF-"]
+    font_size = get_value("-FONT_SIZE-", values)
+    color = values["-COLORS-"]
+    x, y = get_value("-TEXT-X-", values), get_value("-TEXT-Y-", values)
+    text = values["-TEXT-"]
 
     if image_file and os.path.exists(image_file):
         shutil.copy(image_file, tmp_file)
@@ -46,13 +46,14 @@ def apply_text(values, window):
             image.save(tmp_file)
 
         photo_img = ImageTk.PhotoImage(image)
-        window["image"].update(data=photo_img)
+        window["-IMAGE-"].update(data=photo_img)
 
 
 def create_row(label, key, file_types):
     return [
         sg.Text(label),
-        sg.Input(size=(25, 1), enable_events=True, key=key),
+        sg.Input(size=(25, 1), enable_events=True, key=key,
+                 readonly=True),
         sg.FileBrowse(file_types=file_types),
     ]
 
@@ -74,7 +75,7 @@ def save_image(values):
     save_filename = sg.popup_get_file(
         "File", file_types=file_types, save_as=True, no_window=True
     )
-    if save_filename == values["filename"]:
+    if save_filename == values["-FILENAME-"]:
         sg.popup_error(
             "You are not allowed to overwrite the original image!")
     else:
@@ -88,7 +89,7 @@ def update_ttf_values(window):
     if directory is not None:
         ttf_files = get_ttf_files(directory)
         new_values = list(ttf_files.keys())
-        window["ttf"].update(values=new_values,
+        window["-TTF-"].update(values=new_values,
                              value=new_values[0])
 
 
@@ -101,41 +102,40 @@ def main():
 
     elements = [
         [sg.Menu(menu_items)],
-        [sg.Image(key="image")],
-        create_row("Image File:", "filename", file_types),
-        [sg.Text("Text:"), sg.Input(key="text", enable_events=True)],
+        [sg.Image(key="-IMAGE-")],
+        create_row("Image File:", "-FILENAME-", file_types),
+        [sg.Text("Text:"), sg.Input(key="-TEXT-", enable_events=True)],
         [
             sg.Text("Text Position"),
             sg.Text("X:"),
             sg.Input("10", size=(5, 1), enable_events=True,
-                     key="text-x"),
+                     key="-TEXT-X-"),
             sg.Text("Y:"),
             sg.Input("10", size=(5, 1), enable_events=True,
-                     key="text-y"),
+                     key="-TEXT-Y-"),
         ],
         [
-            sg.Combo(colors, default_value=colors[0], key='colors',
-                     enable_events=True),
-            sg.Combo(ttf_filenames, default_value=ttf_filenames[0], key='ttf',
-                     enable_events=True),
+            sg.Combo(colors, default_value=colors[0], key='-COLORS-',
+                     enable_events=True, readonly=True),
+            sg.Combo(ttf_filenames, default_value=ttf_filenames[0],
+                     key='-TTF-', enable_events=True, readonly=True),
             sg.Text("Font Size:"),
-            sg.Input("12", size=(5, 1), key="font_size", enable_events=True),
+            sg.Input("12", size=(5, 1), key="-FONT_SIZE-", enable_events=True),
 
         ],
-        [sg.Button("Save Image", enable_events=True,
-                   key="save")],
+        [sg.Button("Save Image", enable_events=True)],
     ]
 
-    window = sg.Window("Draw Text GUI", elements)
+    window = sg.Window("Draw Text GUI", elements, size=(500, 500))
 
     while True:
         event, values = window.read()
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
-        if event in ["filename", "colors", "ttf", "font_size",
-                     "text-x", "text-y", "text"]:
+        if event in ["-FILENAME-", "-COLORS-", "-TTF-", "-FONT_SIZE-",
+                     "-TEXT-X-", "-TEXT-Y-", "-TEXT-"]:
             apply_text(values, window)
-        if event == "save" and values["filename"]:
+        if event == "Save Image" and values["-FILENAME-"]:
             save_image(values)
         if event == "Open Font Directory":
             update_ttf_values(window)
