60768217b78947b8795dc34456afdf29b4ae4c7b
Mike Driscoll <mike@pythonlibrary.org>
2021-02-26 15:35:24 -0600

    fix spacing issue

--- 12_integration/tk_pillow_demo.py
+++ 12_integration/tk_pillow_demo.py
@@ -8,7 +8,7 @@ def main():
     root.title("Tkinter Image Viewer")
     pil_img = Image.open("pink_flower.jpg")
     width, height = pil_img.size
-    canvas = Canvas(root, width = width, height = height)
+    canvas = Canvas(root, width=width, height=height)
     canvas.pack()
     img = ImageTk.PhotoImage(pil_img)
     canvas.create_image(20, 20, anchor=NW, image=img)
